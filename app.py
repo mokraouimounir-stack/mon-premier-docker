8a4e000a57aabe1e2196beb67c5af493012b1477
MOKRAOUI <mokraouimounir2020@gmail.com>
2026-09-24 23:52:51 +0200

    Modification du message de bienvenue

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'Hello depuis mon conteneur Docker !')
+        self.wfile.write(b'Hello depuis mon pipeline CI/CD !')
 
 HTTPServer(('0.0.0.0', 8000), Handler).serve_forever()
