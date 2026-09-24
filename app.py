7b6c8993809ef05635ff8d5d56dcb643c483c794
MOKRAOUI <mokraouimounir2020@gmail.com>
2026-09-24 11:45:01 +0200

    Premier commit : appli Docker avec docker-compose

---
+++ app.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello depuis mon conteneur Docker !')
+
+HTTPServer(('0.0.0.0', 8000), Handler).serve_forever()
